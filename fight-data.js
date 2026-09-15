c1499e1fcf59f3eda4976513db5c466f36bd6c87
Mrwolfie2410 <tblxshadow@gmail.com>
2026-09-15 18:56:57 +0100

    Add central Mrs Wolfie fight data

---
+++ fight-data.js
@@ -0,0 +1,188 @@
+/* =========================================================
+   MRS WOLFIE
+   CENTRAL FIGHT DATA
+========================================================= */
+
+window.MRS_WOLFIE_FIGHTS = {
+
+
+  /* =======================================================
+     CURRENT / NEXT FIGHT
+  ======================================================== */
+
+  nextFight: {
+
+    promotion:
+      "AYRSHIRE BOXING",
+
+    event:
+      "THE LAUNCH",
+
+    opponent:
+      "COLLEEN LEYDEN",
+
+    date:
+      "2026-10-17",
+
+    dateDisplay:
+      "17 OCTOBER 2026",
+
+    venue:
+      "AYR RACECOURSE",
+
+    status:
+      "UPCOMING",
+
+    type:
+      "FIGHT NIGHT"
+
+  },
+
+
+
+  /* =======================================================
+     UPCOMING FIGHTS
+  ======================================================== */
+
+  upcoming: [
+
+
+    {
+
+      promotion:
+        "AYRSHIRE BOXING",
+
+      event:
+        "THE LAUNCH",
+
+      opponent:
+        "COLLEEN LEYDEN",
+
+      date:
+        "2026-10-17",
+
+      dateDisplay:
+        "17 OCTOBER 2026",
+
+      venue:
+        "AYR RACECOURSE",
+
+      status:
+        "NEXT FIGHT"
+
+    },
+
+
+    {
+
+      promotion:
+        "TRAIN4FIGHT",
+
+      event:
+        "SUPER MIDDLEWEIGHT TITLE",
+
+      opponent:
+        "TBA",
+
+      date:
+        "2026-11-20",
+
+      dateEnd:
+        "2026-11-21",
+
+      dateDisplay:
+        "20/21 NOVEMBER 2026",
+
+      venue:
+        "EASTHOUSES MINERS CLUB",
+
+      status:
+        "UPCOMING"
+
+    }
+
+
+  ],
+
+
+
+  /* =======================================================
+     FIGHT HISTORY
+  ======================================================== */
+
+  history: [
+
+
+    {
+
+      promotion:
+        "TRAIN 4 FIGHT PROMOTIONS",
+
+      opponent:
+        "KATIE FAE THE BING",
+
+      date:
+        "2026-03-14",
+
+      dateDisplay:
+        "14 MARCH 2026",
+
+      venue:
+        "EASTHOUSES MINERS CLUB",
+
+      result:
+        "W"
+
+    },
+
+
+    {
+
+      promotion:
+        "EBO",
+
+      opponent:
+        "BRY KELLY",
+
+      date:
+        "2025-07-05",
+
+      dateDisplay:
+        "05 JULY 2025",
+
+      venue:
+        "TRILOGY NIGHTCLUB",
+
+      result:
+        "L"
+
+    },
+
+
+    {
+
+      promotion:
+        "EBO",
+
+      opponent:
+        "DEMI WISE",
+
+      date:
+        "2025-05-25",
+
+      dateDisplay:
+        "25 MAY 2025",
+
+      venue:
+        "TRILOGY NIGHTCLUB",
+
+      result:
+        "L"
+
+    }
+
+
+  ]
+
+
+};
